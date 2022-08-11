0c379ad6a7d63e6f43cb689f395fcc0fa0705a74
Dan Jordan <dan.jordan@msi-inc.com>
2022-08-11 12:40:20 -0400

    add worker

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ set :repo_url,        'git@bitbucket.org:msi_ims/kamp.git'
 set :application,     'mesp'
 set :user,            'deployer'
 set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :puma_workers,    1
 
 # Don't change these unless you know what you're doing
 set :pty,             true
