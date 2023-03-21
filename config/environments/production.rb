5f83aa79ac1c6f53eba11f300d0daf9e0737f08a
Daniel Jordan <dan.jordan@msi-inc.com>
2023-03-21 15:10:13 -0400

    change url for cutover

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Mesp::Application.configure do
   config.active_support.deprecation = :notify
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'phase1.jordankmportal.com' }
+  config.action_mailer.default_url_options = { :host => 'jordankmportal.com' }
   config.action_mailer.delivery_method = :ses
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
