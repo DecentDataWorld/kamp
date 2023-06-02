cb9b3804e19300076496ee39bbb1afdaf29c21c6
Dan Jordan <dan.jordan@msi-inc.com>
2023-06-01 21:25:14 -0400

    CS-1056: rails 7, esbuild, css bundling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  mount Ckeditor::Engine => '/ckeditor'
+  #mount Ckeditor::Engine => '/ckeditor'
   resources :banners
 
   get 'survey/index'
