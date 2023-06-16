9b0c1fd6a2011f609fcfe54234102e631eabf32f
mishka.mukherji <mishka.mukherji@tetratech.com>
2023-06-16 16:55:32 -0400

    CS-1064: remove unneeded model rule

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,6 @@ class Event < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :cop, optional: true
-  has_and_belongs_to_many :tags
 
   validates_presence_of :name, :message => "Name is required"
 end
