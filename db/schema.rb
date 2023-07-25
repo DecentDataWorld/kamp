800f66228b46927d54726c670169a0dc3768dea3
Daniel Jordan <dan.jordan@msi-inc.com>
2023-07-25 16:20:32 -0400

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2023_07_25_133953) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_181318) do
->>>>>>> integration/railsupgrade
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
