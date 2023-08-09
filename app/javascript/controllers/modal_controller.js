0f88bb92e26ef18bcbda05058219782fcc3cf7d3
Daniel Jordan <dan.jordan@msi-inc.com>
2023-08-09 10:08:18 -0400

    CS-1116: hack to prevent modal staying open

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -5,10 +5,14 @@ export default class extends Controller {
   connect() {
     this.modal = new Modal(this.element)
     this.modal.show()
+    this.element.addEventListener('hidden.bs.modal', (event) => {
+      location.reload();
+    })
   }
 
   hideBeforeRender(event) {
     if (this.isOpen()) {
+      console.log("hideBeforeRender - isOpen");
       event.preventDefault()
       this.element.addEventListener('hidden.bs.modal', event.detail.resume)
       this.modal.hide()
