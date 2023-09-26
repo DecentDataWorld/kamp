d22d0fca8e2e177c446768dd82e638f4dc7df511
mishka.mukherji <mishka.mukherji@tetratech.com>
2023-09-26 15:46:23 -0400

    CS-1192: denial reasons, part 1, finally

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,96 +1,96 @@
 class TagsController < ApplicationController
-    before_action :set_tag, only: [:edit, :update, :destroy]
-    before_action :authorized?
-  
-    # GET /tags
-    # GET /tags.json
-    def index
-      @tags = Tag.includes(:tag_type).order(:name)
-  
-      respond_to do |format|
-        format.html
-        format.json { render json: @tags.order(:name), status: :ok }
-      end
-    end
-  
-    def grouped_tags
-      @tags = Tag.grouped_tags
-      render json: @tags, status: :ok
-    end
-  
-  
-    # GET /tags/new
-    def new
-      @tag = Tag.new
-    end
-  
-    # GET /tags/1/edit
-    def edit
-    end
-  
-    # POST /tags
-    # POST /tags.json
-    def create
-      @tag = Tag.new(tag_params)
-  
-      respond_to do |format|
-        if @tag.save
-          flash[:notice] = I18n.t("notices.create_success")
-          format.html { redirect_to tags_path }
-          format.json { render :show, status: :created, location: @tag }
-        else
-          format.html { render :new, status: :unprocessable_entity }
-          format.json { render json: @tag.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    # PATCH/PUT /tags/1
-    # PATCH/PUT /tags/1.json
-    def update
-      respond_to do |format|
-        if @tag.update(tag_params)
-          flash[:notice] = I18n.t("notices.update_success")
-          format.html { redirect_to tags_path }
-          format.json { render :show, status: :ok, location: @tag }
-        else
-          format.html { render :edit, status: :unprocessable_entity }
-          format.json { render json: @tag.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    # DELETE /tags/1
-    # DELETE /tags/1.json
-    def destroy
-      respond_to do |format|
-        if @tag.destroy
-          flash[:notice] = I18n.t("notices.delete_success")
-          format.html { redirect_to tags_path }
-          format.json { head :no_content }
-        else
-          format.html { render :edit }
-          format.json { render json: @tag.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    private
-      # Use callbacks to share common setup or constraints between actions.
-      def set_tag
-        @tag = Tag.find(params[:id])
-      end
+  before_action :set_tag, only: [:edit, :update, :destroy]
+  before_action :authorized?
 
-      def authorized?
-        unless (can? :manage, :all)
-          flash[:error] = "You are not authorized to view that page."
-          redirect_to root_path
-        end
+  # GET /tags
+  # GET /tags.json
+  def index
+    @tags = Tag.includes(:tag_type).order(:name)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @tags.order(:name), status: :ok }
+    end
+  end
+
+  def grouped_tags
+    @tags = Tag.grouped_tags
+    render json: @tags, status: :ok
+  end
+
+
+  # GET /tags/new
+  def new
+    @tag = Tag.new
+  end
+
+  # GET /tags/1/edit
+  def edit
+  end
+
+  # POST /tags
+  # POST /tags.json
+  def create
+    @tag = Tag.new(tag_params)
+
+    respond_to do |format|
+      if @tag.save
+        flash[:notice] = I18n.t("notices.create_success")
+        format.html { redirect_to tags_path }
+        format.json { render :show, status: :created, location: @tag }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
       end
-  
-      # Only allow a list of trusted parameters through.
-      def tag_params
-        params.require(:tag).permit(:name, :tag_type_id)
+    end
+  end
+
+  # PATCH/PUT /tags/1
+  # PATCH/PUT /tags/1.json
+  def update
+    respond_to do |format|
+      if @tag.update(tag_params)
+        flash[:notice] = I18n.t("notices.update_success")
+        format.html { redirect_to tags_path }
+        format.json { render :show, status: :ok, location: @tag }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  # DELETE /tags/1
+  # DELETE /tags/1.json
+  def destroy
+    respond_to do |format|
+      if @tag.destroy
+        flash[:notice] = I18n.t("notices.delete_success")
+        format.html { redirect_to tags_path }
+        format.json { head :no_content }
+      else
+        format.html { render :edit }
+        format.json { render json: @tag.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tag
+      @tag = Tag.find(params[:id])
+    end
+
+    def authorized?
+      unless (can? :manage, :all)
+        flash[:error] = "You are not authorized to view that page."
+        redirect_to root_path
+      end
+    end
+
+    # Only allow a list of trusted parameters through.
+    def tag_params
+      params.require(:tag).permit(:name, :tag_type_id)
+    end
   end
   
