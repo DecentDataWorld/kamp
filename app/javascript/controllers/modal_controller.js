d2b615002d46cad62b072d5152b860f07f9fa9f2
mishka.mukherji <mishka.mukherji@tetratech.com>
2023-09-27 21:02:53 -0400

    CS-1143: prevent page reload on modal close, introduce another hack to prevent modal showing

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -5,18 +5,10 @@ export default class extends Controller {
   connect() {
     this.modal = new Modal(this.element)
     this.modal.show()
-    //this is to prevent the modal from opening when going back to the page; kind of hacky - prob a better way to do this
-    this.element.addEventListener('hidden.bs.modal', (event) => {
-      location.reload();
-    })
   }
 
   hideBeforeRender(event) {
-    if (this.isOpen()) {
-      event.preventDefault()
-      this.element.addEventListener('hidden.bs.modal', event.detail.resume)
-      this.modal.hide()
-    }
+    this.element.removeAttribute('class')
   }
 
   isOpen() {
