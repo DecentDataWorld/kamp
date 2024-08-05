8beff96b30f918f56d56a8e970cb306f889aef5b
Daniel Jordan <dan.jordan@msi-inc.com>
2024-08-05 09:57:29 -0400

    CS-1491: clean

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -166,30 +166,29 @@ class SearchesController < ApplicationController
     @languages = {:arabic => 'Arabic', :english => 'English'}
     @days_backs = {7 => 'Less Than a Week Ago', 30 => 'Less Than a Month Ago', 183 => 'Less Than Six Months Ago', 365 => 'Less Than a Year Ago'}
 
-      resource_results = Resource.search_kmp(params[:query], params[:tags], params[:organization_id], params[:cop_id], params[:language], params[:days_back])
-      @resource_count = resource_results[:count]
-      @resources = Resource.where(id: resource_results[:ids]).order(Arel.sql("array_position(ARRAY[#{resource_results[:ids].join(',')}], resources.id)")).paginate(page: params[:page], per_page: 10)
-      @tags = Resource.search_tags(params[:query], params[:tags], params[:organization_id], params[:cop_id], params[:language], params[:days_back])
-      @orgs = []
-      @organization = nil
-      if params[:organization_id].present? and Organization.where(id: params[:organization_id]).exists?
-        @organization =  Organization.find(params[:organization_id])
-      else
-        @orgs = Resource.search_orgs(params[:query], params[:tags], params[:cop_id], params[:language], params[:days_back])
-      end
+    resource_results = Resource.search_kmp(params[:query], params[:tags], params[:organization_id], params[:cop_id], params[:language], params[:days_back])
+    @resource_count = resource_results[:count]
+    @resources = Resource.where(id: resource_results[:ids]).order(Arel.sql("array_position(ARRAY[#{resource_results[:ids].join(',')}], resources.id)")).paginate(page: params[:page], per_page: 10)
+    @tags = Resource.search_tags(params[:query], params[:tags], params[:organization_id], params[:cop_id], params[:language], params[:days_back])
+    @orgs = []
+    @organization = nil
+    if params[:organization_id].present? and Organization.where(id: params[:organization_id]).exists?
+      @organization =  Organization.find(params[:organization_id])
+    else
+      @orgs = Resource.search_orgs(params[:query], params[:tags], params[:cop_id], params[:language], params[:days_back])
+    end
 
-      @cops = []
-      @cop = nil
-      if params[:cop_id].present? and Cop.where(id: params[:cop_id]).exists?
-        @cop =  Cop.find(params[:cop_id])
-      else
-        @cops = Resource.search_cops(params[:query], params[:tags], params[:organization_id], params[:language], params[:days_back])
-      end
+    @cops = []
+    @cop = nil
+    if params[:cop_id].present? and Cop.where(id: params[:cop_id]).exists?
+      @cop =  Cop.find(params[:cop_id])
+    else
+      @cops = Resource.search_cops(params[:query], params[:tags], params[:organization_id], params[:language], params[:days_back])
+    end
 
-      collection_results = Collection.search_kmp(params[:query], params[:tags], params[:organization_id], params[:cop_id], params[:days_back])
-      @collection_count = collection_results[:count]
-      @collections = Collection.where(id: collection_results[:ids]).order(Arel.sql("array_position(ARRAY[#{collection_results[:ids].join(',')}], collections.id)")).paginate(page: params[:collection_page], per_page: 10)
-
+    collection_results = Collection.search_kmp(params[:query], params[:tags], params[:organization_id], params[:cop_id], params[:days_back])
+    @collection_count = collection_results[:count]
+    @collections = Collection.where(id: collection_results[:ids]).order(Arel.sql("array_position(ARRAY[#{collection_results[:ids].join(',')}], collections.id)")).paginate(page: params[:collection_page], per_page: 10)
 
   end
 
